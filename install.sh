38731de933997fd38f3fdf143b09673f7dba5a01
Luis Velez <lvelezsantos@gmail.com>
2020-12-19 14:56:55 -0500

    clean compose file

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 docker-compose build
 docker-compose up -d
 docker-compose exec api bash -c "python manage.py migrate"
-docker-compose exec api bash -c "python manage.py loaddata initial_data.json"
+docker-compose exec api bash -c "python manage.py loaddatad initial_data.json"
